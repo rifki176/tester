1a708fbdd10b6db8b6bb4cd757354e8a88721c6e
rifki <rifki997@gmail.com>
2020-08-29 11:02:45 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sakadaek from "./componen/Sakadaek"
+function App() {
+  return (
+    <div className="App">
+        <Sakadaek/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componen/Sakadaek.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../styles/Sakadaek.css"
+const Sakadaek   = () => {
+    const ContentText= 'MACOK EVERYWHERE';
+    return (<div className="awak">
+        
+        <div className ="header">warning
+        <p>Created by-</p>
+        <p>Rifki</p></div>
+    <div className="container">
+        <div className="text-wrapper">
+<div className="text-1 text">{ContentText}</div>
+            <div className="text-2 text">{ContentText}</div>
+            <div className="text-3 text">{ContentText}</div>
+            <div className="text-4 text">{ContentText}</div>
+            <div className="text-5 text">{ContentText}</div>
+            <div className="text-6 text">{ContentText}</div>
+            <div className="text-7 text">{ContentText}</div>
+            <div className="text-8 text">{ContentText}</div>
+            <div className="text-9 text">{ContentText}</div>
+            <div className="text-10 text">{ContentText}</div>
+            <div className="text-11 text">{ContentText}</div>
+            
+            </div></div></div>);
+
+}
+ 
+ 
+export default Sakadaek;
