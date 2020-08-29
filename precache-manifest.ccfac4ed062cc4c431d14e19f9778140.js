4d36ccd2c9bc0f8e9d570f6680402da5e390e287
rifki <rifki997@gmail.com>
2020-08-29 11:34:16 +0700

    Updates

---
+++ precache-manifest.ccfac4ed062cc4c431d14e19f9778140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3113883666382a2d1a866e916ec60d",
+    "url": "/tester/index.html"
+  },
+  {
+    "revision": "6510d81127eefc1b00d7",
+    "url": "/tester/static/css/main.fff978d1.chunk.css"
+  },
+  {
+    "revision": "d24c6e3e06db30f0b00e",
+    "url": "/tester/static/js/2.19ca2c7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tester/static/js/2.19ca2c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6510d81127eefc1b00d7",
+    "url": "/tester/static/js/main.0992f1a7.chunk.js"
+  },
+  {
+    "revision": "3d806f167633662b8301",
+    "url": "/tester/static/js/runtime-main.d0916054.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tester/static/media/logo.5d5d9eef.svg"
+  }
+]);
